ac21e9d05b6cacc8605705d790eeab053556986b
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-15 23:52:41 +0100

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
+// If you want to support toggling dark mode manually
+// instead of relying on the operating system preference,
+//  use the selector strategy instead of the media strategy:
+
+import flowbite from 'flowbite/plugin';
 const config: Config = {
   darkMode: 'selector',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/flowbite-react/lib/**/*.js',
+    './node_modules/flowbite/**/*.js',
+    './public/**/*.html',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+  plugins: [flowbite],
 };
 export default config;
