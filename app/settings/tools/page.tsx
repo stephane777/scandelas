f599f9b7ac8ed1aaefca1333f2fb842dd84b0fc6
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-20 17:05:21 +0100

    fix Vercel warning

--- app/settings/tools/page.tsx
+++ app/settings/tools/page.tsx
@@ -1,3 +1,21 @@
+import Search from '@/app/ui/Search';
+import { CreateTool } from '@/app/ui/Settings/Buttons';
+import ToolTab from '@/app/ui/Settings/ToolsTab';
+import { ToolsTableSkeleton } from '@/app/ui/Skeletons';
+import { Suspense } from 'react';
 export default async function Page() {
-  return <div>{'Settings / tools'}</div>;
+  return (
+    <main className="mx-auto min-h-screen max-w-6xl p-6">
+      <section className="flex-column items-center justify-center pt-16">
+        <h1 className={`mb-4 flex justify-center text-2xl md:text-2xl`}>Tools</h1>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Search invoices..." />
+          <CreateTool />
+        </div>
+        <Suspense fallback={<ToolsTableSkeleton />}>
+          <ToolTab />
+        </Suspense>
+      </section>
+    </main>
+  );
 }
