f0b80577612775615cfd6fb625335b222a5b1cd4
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-23 23:24:55 +0100

    feat: update  Buttons component

--- app/ui/Settings/Buttons.tsx
+++ app/ui/Settings/Buttons.tsx
@@ -2,13 +2,13 @@ import { PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 // import { deleteInvoice } from '@/app/lib/actions';
 
-export function CreateTool() {
+export function CreateButton({ pathToCreate, name }: { pathToCreate: string; name: string }) {
   return (
     <Link
-      href="/dashboard/invoices/create"
+      href={pathToCreate}
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
-      <span className="hidden md:block">Create Tool</span> <PlusIcon className="h-5 md:ml-4" />
+      <span className="hidden md:block">{name}</span> <PlusIcon className="h-5 md:ml-4" />
     </Link>
   );
 }
