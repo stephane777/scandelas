c6db5a2e6423593d94044c11109e012c1fd3c27d
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-25 19:02:21 +0100

    fix: wrap Search component with Suspense

--- app/settings/tools/page.tsx
+++ app/settings/tools/page.tsx
@@ -23,11 +23,11 @@ export default async function Page({
     <main className="mx-auto flex min-h-screen max-w-6xl justify-center p-6">
       <section className="flex-column w-[800px] items-center justify-center pt-16">
         <h1 className={`mb-4 flex justify-center text-2xl md:text-2xl`}>Tools</h1>
-        <div className="pb mt-4 flex items-center justify-between gap-2 pb-12 md:mt-8">
-          <Search placeholder="Search tools..." />
-          <CreateButton pathToCreate={'/settings/tools/create'} name={'create tool'} />
-        </div>
         <Suspense fallback={<div>Loading...</div>}>
+          <div className="pb mt-4 flex items-center justify-between gap-2 pb-12 md:mt-8">
+            <Search placeholder="Search tools..." />
+            <CreateButton pathToCreate={'/settings/tools/create'} name={'create tool'} />
+          </div>
           <Table query={query} currentPage={currentPage} />
         </Suspense>
         <div className="mt-5 flex w-full justify-center">
