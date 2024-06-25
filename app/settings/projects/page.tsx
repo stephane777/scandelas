23c470868e284f8de4470997d0c85a9e3f903afb
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-25 19:14:22 +0100

    fix: wrap both Search component in project & tools with Suspense

--- app/settings/projects/page.tsx
+++ app/settings/projects/page.tsx
@@ -11,8 +11,10 @@ export default async function Page() {
       <section className="flex-column items-center pt-16">
         <h1 className={`mb-4 flex justify-center text-2xl md:text-2xl`}>Projects</h1>
         <div className="pb mt-4 flex items-center justify-between gap-2 pb-12 md:mt-8">
-          <Search placeholder="Search projects..." />
-          <CreateButton pathToCreate={'/settings/projects/create'} name={'create project'} />
+          <Suspense>
+            <Search placeholder="Search projects..." />
+            <CreateButton pathToCreate={'/settings/projects/create'} name={'create project'} />
+          </Suspense>
         </div>
         <Suspense fallback={<ProjectTableSkeleton />}>
           <ProjectsTab />
