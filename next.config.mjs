61c194bf09be8fdf3d357f11865aab108cd2eceb
Stephane Candelas <stephanecandelas@gmail.com>
2024-06-28 23:11:33 +0100

    feat: update next config with remotePatterns to handle remote images optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**/www.codewars.com',
+        port: '',
+        pathname: '/users/stephane777/badges/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+// https://www.codewars.com/users/stephane777/badges/large
